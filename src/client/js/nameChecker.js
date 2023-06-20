e28f80cbacd2e74534799f942e30f753a771ed78
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-20 14:24:24 +0200

    styling update

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,55 +1,73 @@
 function analyzeSentiment() {
-    fetch('http://localhost:8081/api/apikey')
-      .then(response => response.json())
-      .then(data => {
-        const apiKey = data.apiKey; // API-Schlüssel abrufen
-        console.log(apiKey); // API-Schlüssel verwenden
-  
-        //const text = 'Ich liebe dieses Produkt! Es ist großartig!';
+  fetch('http://localhost:8081/api/apikey')
+    .then(response => response.json())
+    .then(data => {
+      const apiKey = data.apiKey; // API-Schlüssel abrufen
+      console.log(apiKey); // API-Schlüssel verwenden
 
-        let formText = document.getElementById('name').value
+      let formText = document.getElementById('name')
 
-    
-        const url = 'https://api.meaningcloud.com/sentiment-2.1';
-  
-        const params = new URLSearchParams();
-        params.append('key', apiKey);
-        params.append('txt', formText);
-        params.append('lang', 'de');
-  
-        return fetch(url, {
-          method: 'POST',
-          body: params
-        });
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Sentiment Score:', data.score_tag);
-        console.log('Agreement:', data.agreement);
-        console.log('Subjectivity:', data.subjectivity);
-        console.log('Confidence:', data.confidence);
-        document.getElementById('results').innerHTML = data.subjectivity
-      })
-      .catch(error => {
-        console.error('Fehler:', error);
+
+      // Den Inhalt des Elements in eine Variable speichern
+      var paragraphInhalt = formText.textContent;
+
+      const url = 'https://api.meaningcloud.com/sentiment-2.1';
+
+      const params = new URLSearchParams();
+      params.append('key', apiKey);
+      params.append('txt', paragraphInhalt);
+      params.append('lang', 'en');
+
+      return fetch(url, {
+        method: 'POST',
+        body: params
       });
+    })
+    .then(response => response.json())
+    .then(data => {
+      // console.log('Sentiment Score:', data.score_tag);
+      // console.log('Agreement:', data.agreement);
+      // console.log('Subjectivity:', data.subjectivity);
+      // console.log('Confidence:', data.confidence);
+
+      // Polarity ausgeben
+      const polarity = getPolarity(data.score_tag);
+      document.getElementById('polarity').innerHTML = polarity;
+      document.getElementById('subjectivity').innerHTML = data.subjectivity;
+
+      let snippedText = data.sentence_list[0].text.split(' ').slice(0, 10).join(' ');
+
+      document.getElementById('textSnip').innerHTML = snippedText + '...';
+    })
+    .catch(error => {
+      console.error('Fehler:', error);
+    });
+}
+
+function getPolarity(scoreTag) {
+  if (scoreTag === 'P+' || scoreTag === 'P') {
+    return 'positive';
+  } else if (scoreTag === 'N+' || scoreTag === 'N') {
+    return 'negative';
+  } else {
+    return 'neutral';
   }
-  
-  function checkForName(inputText) {
-    analyzeSentiment();
-    console.log("::: Running checkForName :::", inputText);
-    let names = [
-      "Picard",
-      "Janeway",
-      "Kirk",
-      "Archer",
-      "Georgiou"
-    ];
-  
-    if (names.includes(inputText)) {
-      alert("Welcome, Captain!");
-    }
+}
+
+function checkForName(inputText) {
+  analyzeSentiment();
+  console.log("::: Running checkForName :::", inputText);
+  let names = [
+    "Picard",
+    "Janeway",
+    "Kirk",
+    "Archer",
+    "Georgiou"
+  ];
+
+  if (names.includes(inputText)) {
+    alert("Welcome, Captain!");
   }
-  
-  export { checkForName };
-  
+}
+
+export { checkForName };
