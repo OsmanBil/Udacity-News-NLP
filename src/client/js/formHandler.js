a7b3011d8e22aefc44cfce07ecbb63ed5dd0d079
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-21 20:37:38 +0200

    update

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,16 @@
 function handleSubmit(event) {
     event.preventDefault();
-    Client.analyzeSentiment(); 
-}
 
+    const inputField = document.getElementById("name");
+    const inputValue = inputField.value.trim();
+
+    // Check if input value is empty
+    if (inputValue === "") {
+      alert("Please enter a text.");
+      return false;
+    }
+    Client.analyzeSentiment();
+    return true;
+}
 
 export { handleSubmit };
